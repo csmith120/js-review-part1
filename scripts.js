b6dca91094db777b0dc945616e06d7162eac00c1
CSmith3s <CSmith3s@capetigers.com>
2024-08-26 14:22:46 -0500

    understanding

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
-const header = document.querySelector('h1');
+/*const header = document.querySelector('h1');
 //header.innerText = 'my new text!' 
 
 function changeText() {
     header.innerText = 'my new text';
 }
-
+//exercise 1
 console.log("Hello, World.")
+
+//exercie 2
+const firstName = "christian"
+
+console.log(firstName)
+
+exercise 3
+const side1 = 10,
+side3 = 10,
+side2 = 30,
+side4 = 30;
+
+console.log ((side1 + side3) * (side2 + side4))*/
+
+//exercise 4
